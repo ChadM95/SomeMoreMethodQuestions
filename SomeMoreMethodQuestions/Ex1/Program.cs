074c45c40ecb5cc70d85753ff15d09910bab8321
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-10-19 16:55:15 +0100

    add ex1

--- SomeMoreMethodQuestions/Ex1/Program.cs
+++ SomeMoreMethodQuestions/Ex1/Program.cs
@@ -4,7 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Menu();
+        Console.ReadKey();
+    }
+
+    static void Menu()
+    {
+        Console.WriteLine("Press 1 for Sales");
+        Console.WriteLine("Press 2 for Service");
+        Console.WriteLine("Press 3 to hold for an operator");
     }
 }
 
