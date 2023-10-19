f0c4d28aa803df04f1e1b5853a6292ef7919dd61
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-10-19 17:23:42 +0100

    add ex4

---
+++ SomeMoreMethodQuestions/Ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int volume;
+
+        volume = GetVolume(10,5,3);
+
+        Console.WriteLine(volume);
+
+        Console.ReadKey();
+    }
+
+    static int GetVolume(int length, int width, int height)
+    {
+        return length * width * height;
+    }
+}
+
