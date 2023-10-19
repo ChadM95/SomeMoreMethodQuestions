6f387cc8d58c8651d73e479e63ef372e1609ad95
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-10-19 16:59:35 +0100

    add ex2

---
+++ SomeMoreMethodQuestions/Ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayHi("Mary");
+        SayHi("John");
+        SayHi("Sally");
+        Console.ReadKey();
+    }
+
+    static void SayHi(string name)
+    {
+        Console.WriteLine($"Hi, {name}. I hope you are well today.");
+    }
+}
